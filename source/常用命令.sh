a92f57518f39cb3f7a4f4e39c781a09acef93cd2
diaoguangqiang <diaoguangqiang@126.com>
2017-04-18 11:08:16 +0800

    add 常用命令

---
+++ source/常用命令.sh
@@ -0,0 +1,7 @@
+sudo mount -t vboxsf download /mnt/share
+
+find ./* -name "*.c" | xargs grep "curl_easy_setopt" --color=auto
+
+tcpdump -i eth5 -w sig_2017.01.04_23.35.pcap
+
+route add default gw 10.64.70.254 &
